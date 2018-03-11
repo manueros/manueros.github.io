b9e54bec35839928cafb2bc0dd25b76404000194
Ros <rosario.bellanova@gmail.com>
2018-03-11 20:50:17 +0100

    Added just married on countdown expire

--- js/main.js
+++ js/main.js
@@ -3,22 +3,28 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('2018-04-22T11:00:00').getTime(),
+let countDown = new Date('2018-04-22T11:30:00').getTime(),
   x = setInterval(function() {
 
     let now = new Date().getTime(),
         distance = countDown - now;
-
-    document.getElementById('days').innerHTML = Math.floor(distance / (day)),
-    document.getElementById('hours').innerHTML = Math.floor((distance % (day)) / (hour)),
-    document.getElementById('minutes').innerHTML = Math.floor((distance % (hour)) / (minute)),
-    document.getElementById('seconds').innerHTML = Math.floor((distance % (minute)) / second);
-        
+    
     //do something later when date is reached
-    if (distance < 0) {
+    if (distance <= 0) {
       clearInterval(x);
+      document.getElementById('countdownText').style.display='none',
+      document.getElementById('countdownExpired').style.display='block',
+      document.getElementById('countdownExpired').innerHTML="JUST MARRIED!";
     }
-  
+    else {
+      document.getElementById('countdownText').style.display='block',
+      document.getElementById('countdownExpired').style.display='none',
+      document.getElementById('days').innerHTML = Math.floor(distance / (day)),
+      document.getElementById('hours').innerHTML = Math.floor((distance % (day)) / (hour)),
+      document.getElementById('minutes').innerHTML = Math.floor((distance % (hour)) / (minute)),
+      document.getElementById('seconds').innerHTML = Math.floor((distance % (minute)) / second);
+
+    }
   }, second)
 
 function giftModal(giftId) {
